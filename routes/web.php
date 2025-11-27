3ce4bdd8becbccbb2ea758baa5399f1582fcbc79
Ali Nasser <alinasser.t22@gmail.com>
2025-11-27 15:14:14 +0200

    extract replies markup

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("/feed", function () {
-    $feedItems = json_decode(json_encode([
+function getFeedItems(): mixed
+{
+    return json_decode(json_encode([
         [
             'profile' => [
                 'avatar' => 'images/michael.png',
@@ -22,28 +23,30 @@ Route::get("/feed", function () {
             'likeCount' => 32,
             'replyCount' => 21,
             'repostCount' => 11,
+            'replies' => [
+                [
+                    'profile' => [
+                        'avatar' => 'images/mr-anderson.png',
+                        'display' => 'Mr.Anderson',
+                        'handle' => '@mr_anderson'
+                    ],
+                    'postedDateTime' => '12 mins ago',
+                    'content' => '<p>What A Great Work My Friend!</p>',
+                    'likeCount' => 3,
+                    'replyCount' => 2,
+                    'repostCount' => 1,
+                ]
+            ]
         ],
     ]));
+}
+
+Route::get("/feed", function () {
+    $feedItems = getFeedItems();
     return view("feed", compact('feedItems'));
 });
 
 Route::get("/profile", function () {
-    $feedItems = json_decode(json_encode([
-        [
-            'profile' => [
-                'avatar' => 'images/michael.png',
-                'display' => 'Michael',
-                'handle' => '@mmich_jj'
-            ],
-            'postedDateTime' => '30 mins ago',
-            'content' => sprintf(
-                '<p>I made this! <a>#myartwork</a> <a>#pixel</a></p><img alt="pic-from-michael" src="%s">',
-                asset('images/simon-chilling.png')
-            ),
-            'likeCount' => 32,
-            'replyCount' => 21,
-            'repostCount' => 11,
-        ],
-    ]));
+    $feedItems = getFeedItems();
     return view('profile', compact('feedItems'));
 });
